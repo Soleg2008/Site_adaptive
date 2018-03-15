52cd6cd246f2c3ba2b4f997b22b9eb657a6359db
Soleg2008 <Soleg2008g@gmail.com>
2018-03-15 22:36:26 +0200

    slider

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
-  $('.thumb1').slick({
+  $('.responsive').slick({
+  		arrows: false,
+  		autoplay: false,
+  		dots: true,
+        dotsClass: "dot",
+        focusOnSelect: false,
+        mobileFirst: true,
+        responsive: [
+        {
+      breakpoint: 750,
+      settings: {
+      	autoplay: true      	
+      }
+    }
+    ]
     });
+
 });
