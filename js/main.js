50b6e67e90d12109ca815c38630b43b6767a6077
Soleg2008 <Soleg2008g@gmail.com>
2018-03-25 12:34:02 +0300

    Flex footer 2

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
-$(document).ready(function(){
-  $('.responsive').slick({
-  		arrows: false,
-  		autoplay: false,
-  		dots: true,
+$(document).ready(function () {
+    $('.responsive').slick({
+        arrows: false,
+        autoplay: false,
+        dots: true,
         dotsClass: "dot",
         focusOnSelect: false,
         mobileFirst: true,
         fade: true,
         responsive: [
-        {
-      breakpoint: 750,
-      settings: {
-      	autoplay: true      	
-      }
-    }
-    ]
+            {
+                breakpoint: 750,
+                settings: {
+                    autoplay: true
+                }
+            }
+        ]
     });
-  //выпадающее меню slide toggle переключает дисплей нон-блок\\
-  $('.burger_button').on("click",function(){
-  	$(".header__btm_menu").stop().slideToggle();
-  });
+    //выпадающее меню slide toggle переключает дисплей нон-блок\\
+    $('.burger_button').on("click", function () {
+        $(".header__btm_menu").stop().slideToggle();
+    });
 
-  $('.search_icon').on("click",function(){
-  	$(".form_search").stop().slideToggle();
-  });
+    $('.search_icon').on("click", function () {
+        $(".form_search").stop().slideToggle();
+    });
 
 });
